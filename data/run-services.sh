b12240e339782f37fac15520872a9e8120704689
Kirant400 <kirant400@gmail.com>
2020-08-01 23:40:53 +0400

    Update run-services.sh

--- data/run-services.sh
+++ data/run-services.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 echo '/run-services.sh'
-
+if [ `id -u` -ge 10000 ]; then
+ cat /etc/passwd | sed -e "s/^tcuser:/builder:/" &gt; /tmp/passwd
+ echo "tcuser:x:`id -u`:`id -g`:,,,:/home/tcuser:/bin/bash" &gt;&gt; /tmp/passwd
+ cat /tmp/passwd &gt; /etc/passwd
+ rm /tmp/passwd
+fi
 for entry in /services/*.sh
 do
   if [[ -f "$entry" ]]; then
